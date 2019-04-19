c6780fffd49851332f8eb5fd9bb233d0cc826bf0
Denispok <shakhover@yandex.ru>
2019-04-19 15:36:20 +0300

    Remove Repository logic from MainActivity

--- app/src/main/java/com/fintech/denispok/fintechproject/ui/MainActivity.kt
+++ app/src/main/java/com/fintech/denispok/fintechproject/ui/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.fintech.denispok.fintechproject.ui
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import com.fintech.denispok.fintechproject.R
-import com.fintech.denispok.fintechproject.ui.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,14 +21,13 @@ class MainActivity : AppCompatActivity() {
         const val EVENTS_TAB = 0
         const val COURSES_TAB = 1
         const val PROFILE_TAB = 2
-
-        const val AUTH_REQUEST_CODE = 1
+        const val DEFAULT_TAB = COURSES_TAB
     }
 
     private lateinit var viewPager: ViewPager
     private lateinit var viewPagerAdapter: MainFragmentAdapter
     private lateinit var bottomNavigationView: BottomNavigationView
-    private var currentTab = 0
+    private var currentTab = DEFAULT_TAB
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,14 +44,7 @@ class MainActivity : AppCompatActivity() {
             currentTab = when (item.itemId) {
                 R.id.navigation_events -> EVENTS_TAB
                 R.id.navigation_courses -> COURSES_TAB
-                R.id.navigation_profile -> {
-                    if (hasValidToken()) {
-                        PROFILE_TAB
-                    } else {
-                        startActivityForResult(Intent(this, AuthActivity::class.java), AUTH_REQUEST_CODE)
-                        currentTab
-                    }
-                }
+                R.id.navigation_profile -> PROFILE_TAB
                 else -> return@setOnNavigationItemSelectedListener false
             }
             viewPager.setCurrentItem(currentTab, false)
@@ -68,7 +56,13 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        currentTab = savedInstanceState?.getInt(CURRENT_TAB_KEY, 0) ?: 0
+        currentTab = savedInstanceState?.getInt(CURRENT_TAB_KEY, DEFAULT_TAB) ?: DEFAULT_TAB
+        bottomNavigationView.selectedItemId = when (currentTab) {
+            EVENTS_TAB -> R.id.navigation_events
+            COURSES_TAB -> R.id.navigation_courses
+            PROFILE_TAB -> R.id.navigation_profile
+            else -> DEFAULT_TAB
+        }
         updateTitle()
     }
 
@@ -83,28 +77,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle?) {
         outState?.putInt(CURRENT_TAB_KEY, currentTab)
         super.onSaveInstanceState(outState)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == AUTH_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                currentTab = PROFILE_TAB
-                viewPager.setCurrentItem(currentTab, false)
-                updateTitle()
-            } else {
-                bottomNavigationView.selectedItemId = when (currentTab) {
-                    EVENTS_TAB -> R.id.navigation_events
-                    COURSES_TAB -> R.id.navigation_courses
-                    else -> throw IllegalStateException()
-                }
-            }
-        }
-    }
-
-    private fun hasValidToken(): Boolean {
-        val preferences = getSharedPreferences("cache", Context.MODE_PRIVATE)
-        return preferences.getString("token", null) != null
     }
 
     private fun updateTitle() {
